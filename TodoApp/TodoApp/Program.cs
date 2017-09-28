5b556083b0898c15d101f77a6e56222333945253
GBotond <botond.gorfol@gmail.com>
2017-09-28 15:30:45 +0200

    List tasks finally working

--- TodoApp/TodoApp/Program.cs
+++ TodoApp/TodoApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TodoApp
 {
@@ -10,14 +11,33 @@ namespace TodoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Command Line Todo application \n" +
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Command Line Todo application \n" +
                               "=============================");
-            Console.WriteLine();
-            Console.WriteLine("Command line arguments: \n" +
-                              "-l   Lists all the tasks \n" +
-                              "-a   Adds a new task \n" +
-                              "-r   Removes an task \n" +
-                              "-c   Completes an task");
+                Console.WriteLine();
+                Console.WriteLine("Command line arguments: \n" +
+                                  "-l   Lists all the tasks \n" +
+                                  "-a   Adds a new task \n" +
+                                  "-r   Removes an task \n" +
+                                  "-c   Completes an task");
+            }
+            else if (args.Contains("-l"))
+            {
+                string[] text = File.ReadAllLines("todolist.txt");
+
+                if (text.Length == 0)
+                {
+                    Console.WriteLine("No todos for today! :)");
+                }
+                else
+                {                    
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        Console.WriteLine((i + 1) + " - " + text[i]);
+                    }
+                }
+            }
 
             Console.ReadLine();
         }
