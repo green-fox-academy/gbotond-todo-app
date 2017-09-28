477746a573ef026a30389c2b060e643e61dd81c4
GBotond <botond.gorfol@gmail.com>
2017-09-28 13:08:43 +0200

    Print usage seems to be correct to me

---
+++ TodoApp/TodoApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Command Line Todo application \n" +
+                              "=============================");
+            Console.WriteLine();
+            Console.WriteLine("Command line arguments: \n" +
+                              "-l   Lists all the tasks \n" +
+                              "-a   Adds a new task \n" +
+                              "-r   Removes an task \n" +
+                              "-c   Completes an task");
+
+            Console.ReadLine();
+        }
+    }
+}
